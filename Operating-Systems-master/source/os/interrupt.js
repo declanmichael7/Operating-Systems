9db5705282a035121df7d46dcc43792e584e5be3
declanmichael7 <31606660+declanmichael7@users.noreply.github.com>
2018-09-09 17:17:57 -0400

    Add files via upload
    -Got rid of the c script and created the json.config file to get the compiler to work
    -Added descriptions for the commands in the man command
    -Added the whereami command
    (By the way, this is my first commit, but I have been working steadily on the project, just forgot to upload my progress)

---
+++ Operating-Systems-master/source/os/interrupt.js
@@ -0,0 +1,15 @@
+"use strict";
+/* ------------
+   Interrupt.ts
+   ------------ */
+var TSOS;
+(function (TSOS) {
+    var Interrupt = /** @class */ (function () {
+        function Interrupt(irq, params) {
+            this.irq = irq;
+            this.params = params;
+        }
+        return Interrupt;
+    }());
+    TSOS.Interrupt = Interrupt;
+})(TSOS || (TSOS = {}));
